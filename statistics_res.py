7dc6c91e9a9f2181c48b5b970c05c3408eaa2863
liuzhuorui <245827196@qq.com>
2025-01-06 17:22:13 +0800

    update the ratio res

--- statistics_res.py
+++ statistics_res.py
@@ -26,6 +26,20 @@ def save_result_file(save_path, data, mode):
         f.write(data)
     
 
+def calculate_acc_ratio(duo_res, layer_wise_res):
+    columns = ["1k", "2k", "4k", "8k", "16k", "32k"]
+    index = ["1k", "2k", "4k", "8k", "16k", "32k"]
+    df = pd.DataFrame(columns=columns, index=index)
+    for col in columns:
+        for idx in index:
+            duo_p, duo_d = duo_res.at[idx, col]
+            layer_wise_p, layer_wise_d = layer_wise_res.at[idx, col]
+            res = (
+                duo_p / layer_wise_p, (duo_p - layer_wise_p) / duo_p, duo_d / layer_wise_d, (duo_d - layer_wise_d) / duo_d
+            )
+            df.at[idx, col] = res
+    return df
+
 def main(args):
     duo_res_file_list = glob.glob(args.data_path + "/duo*")
     layer_wise_res_file_list = glob.glob(args.data_path + "/layer*")
@@ -33,13 +47,17 @@ def main(args):
     duo_res = preprocess_data(duo_res_file_list)
     layer_wise_res = preprocess_data(layer_wise_res_file_list)
 
+    acc_ratio = calculate_acc_ratio(duo_res, layer_wise_res)
+
     duo_markdown_res, layer_wise_markdown_res = duo_res.to_markdown(), layer_wise_res.to_markdown()
     print(duo_markdown_res)
     print(layer_wise_markdown_res)
+    print(acc_ratio)
 
     if args.save_path:
         save_result_file(args.save_path, duo_markdown_res, "duo")
         save_result_file(args.save_path, layer_wise_markdown_res, "layer-wise")
+        save_result_file(args.save_path, acc_ratio, "ratio")
 
 
 
